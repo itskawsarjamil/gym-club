fa9e7236e936831e9a8f806d01b7767cebcfeec4
Kawsar Jamil <68264633+itskawsarjamil@users.noreply.github.com>
2022-09-30 17:43:32 +0600

    all question answer and format added

--- src/FAQ/FAQ.js
+++ src/FAQ/FAQ.js
@@ -5,27 +5,26 @@ function BasicExample() {
         <div className='faq'>
             <Accordion defaultActiveKey="0">
                 <Accordion.Item eventKey="0">
-                    <Accordion.Header  className='acrdn-hdr'>How Does React Work ?</Accordion.Header>
+                    <Accordion.Header className='acrdn-hdr'><h5>How Does React Work ?</h5></Accordion.Header>
                     <Accordion.Body>
-                    ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code.
+                        ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code.
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="1">
-                    <Accordion.Header className='acrdn-hdr'>Difference between Props and States</Accordion.Header>
+                    <Accordion.Header className='acrdn-hdr'><h5>Difference between Props and States</h5></Accordion.Header>
                     <Accordion.Body>
-                    State is the local state of the component which cannot be accessed and modified outside of the component. It's equivalent to local variables in a function. Props, on the other hand, make components reusable by giving components the ability to receive data from their parent component in the form of props.
+                        State is the local state of the component which cannot be accessed and modified outside of the component. It's equivalent to local variables in a function. Props, on the other hand, make components reusable by giving components the ability to receive data from their parent component in the form of props.
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="2">
-                    <Accordion.Header className='acrdn-hdr'>Without api fetch what other things useEffect hook can do ?</Accordion.Header>
+                    <Accordion.Header className='acrdn-hdr'><h5>Without api fetch what other things useEffect hook can do ?</h5></Accordion.Header>
                     <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                        aliquip ex ea commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                        culpa qui officia deserunt mollit anim id est laborum.
+                        The useEffect Hook allows you to perform side effects in your components. Some examples of side effects are:<br>
+                        </br>
+                        1.Add an event listener for a button<br></br>
+                        2.Perform an action when state or props change<br></br>
+                        3.Clean up event listeners when the component unmounts.<br></br>
+                        4.timers
                     </Accordion.Body>
                 </Accordion.Item>
             </Accordion>
